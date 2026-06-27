771b62a95c1c071284433220e6fddfd74d70fa97
fliperama86 <fliperama86@gmail.com>
2026-06-27 18:04:23 -0300

    Add LoD issue 23 VI trace

--- librecomp/src/vi.cpp
+++ librecomp/src/vi.cpp
@@ -2,6 +2,15 @@
 #include "recomp.h"
 #include "helpers.hpp"
 
+#ifndef LOD_ENABLE_ISSUE23_TRACE
+#define LOD_ENABLE_ISSUE23_TRACE 0
+#endif
+
+#if LOD_ENABLE_ISSUE23_TRACE
+extern "C" uint32_t lod_current_map_overlay_rom();
+extern "C" int lod_current_map_overlay_load_count();
+#endif
+
 extern "C" void osViSetYScale_recomp(uint8_t* rdram, recomp_context * ctx) {
     osViSetYScale(ctx->f12.fl);
 }
@@ -20,6 +29,22 @@ extern "C" void osViBlack_recomp(uint8_t* rdram, recomp_context* ctx) {
     if (vb_count <= 20 || (uint32_t)ctx->r4 != 0) {
         fprintf(stderr, "[osViBlack] #%d active=%d\n", vb_count, (uint32_t)ctx->r4);
     }
+#if LOD_ENABLE_ISSUE23_TRACE
+    {
+        const uint32_t map_rom = lod_current_map_overlay_rom();
+        if (map_rom == 0x007D4420 || map_rom == 0x0082E330) {
+            static int issue23_vi_count = 0;
+            issue23_vi_count++;
+            if (issue23_vi_count <= 24 || (uint32_t)ctx->r4 != 0 ||
+                (issue23_vi_count % 120) == 0) {
+                fprintf(stderr,
+                        "[ISSUE23_VI_BLACK] #%d vi#%d map#%d map=0x%08X active=%u\n",
+                        issue23_vi_count, vb_count, lod_current_map_overlay_load_count(),
+                        map_rom, (uint32_t)ctx->r4);
+            }
+        }
+    }
+#endif
     osViBlack((uint32_t)ctx->r4);
 }
 
